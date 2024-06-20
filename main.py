6003cb2499acd6d0bb62037bf1fa8fa35ca5adfe
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-19 22:11:08 -0700

    Editing

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+
+    #####################
+    # make your code below
+    # print ('Hello World')
+    #####################
+    print ('Hello World')
+    print ('CS7: Python Programming')
+
+if __name__== '__main__':
+    main()
